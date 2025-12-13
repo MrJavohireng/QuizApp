d4e21f217b045199ff8cd989f644fdb8b16d7362
MrJavohireng <javohiraaaaa1@gmail.com>
2025-12-13 16:14:41 +0500

    Add models SolvedQuestionGroup

--- QuestionApp/admin.py
+++ QuestionApp/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Question, QuestionGroup
+from .models import Question, QuestionGroup, SolvedQuestionGroup
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'option_a', 'option_b', 'option_c', 'option_d', 'correct_option')
     search_fields = ('question_text',)
 @admin.register(QuestionGroup)
 class QuestionGroupAdmin(admin.ModelAdmin):
-    list_display = ("name", "creater")
+    list_display = ("name", "creater", "time")
     search_fields = ('name',"creater")
     filter_horizontal = ('questions',)
+
+@admin.register(SolvedQuestionGroup)
+class SolvedQuestionGroupAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question_group', 'score')
+    search_fields = ('user__user__username', 'question_group__name')
--- QuestionApp/models.py
+++ QuestionApp/models.py
@@ -16,6 +16,15 @@ class QuestionGroup(models.Model):
     name = models.CharField(max_length=100)
     questions = models.ManyToManyField(Question)
     creater=models.ForeignKey(to=Profile, on_delete=models.CASCADE, blank=True, null=True)
+    time=models.IntegerField(default=1)
 
     def __str__(self):
         return f"{self.name}: {self.creater.user.username}"
+    
+class SolvedQuestionGroup(models.Model):
+    user=models.ForeignKey(to=Profile, on_delete=models.CASCADE)
+    question_group=models.ForeignKey(to=QuestionGroup, on_delete=models.CASCADE)
+    score=models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.user.username} - {self.question_group.name} - {self.score}"
